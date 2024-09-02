94f4646daa249a0e6730c2fa5a7e371f94f44942
aesweb <aes.sncr@gmail.com>
2024-09-02 17:09:56 +0300

    exercise 1.4

--- part-1/src/App.jsx
+++ part-1/src/App.jsx
@@ -28,20 +28,20 @@ const Total = ({ parts }) => {
 
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10,
-  };
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7,
-  };
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14,
-  };
-
-  const parts = [part1, part2, part3];
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10,
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7,
+    },
+    {
+      name: 'State of a component',
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
