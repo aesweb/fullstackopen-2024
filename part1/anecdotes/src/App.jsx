e94e84c1f2dc224e020795f09cbb4241b98e3463
aesweb <aes.sncr@gmail.com>
2024-09-02 22:08:11 +0300

    part 1d

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,41 +1,44 @@
 import { useState } from 'react';
 
-const Hello = ({ name, age }) => {
-  const bornYear = () => new Date().getFullYear() - age;
-
-  return (
-    <div>
-      <p>
-        Hello {name}, you are {age} years old
-      </p>
-      <p>So you were probably born in {bornYear()}</p>
-    </div>
-  );
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(' ')}</div>;
 };
 
-const Display = ({ counter }) => <div>{counter}</div>;
-
-const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>;
+const Button = (props) => {
+  console.log('props value is ', props);
+  const { handleClick, text } = props;
+  return <button onClick={handleClick}>{text}</button>;
+};
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  };
 
-  const name = 'Peter';
-  const age = 10;
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
 
   return (
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Display counter={counter} />
-      <Button onSmash={increaseByOne} text="plus" />
-      <Button onSmash={setToZero} text="zero" />
-      <Button onSmash={decreaseByOne} text="minus" />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
     </div>
   );
 };
